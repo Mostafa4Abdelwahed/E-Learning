0926221821dfcf9c9ed5f59f2c013d125648e493
Mostafa Abdelwahed <65525706+Mostafa4Abdelwahed@users.noreply.github.com>
2024-06-27 08:17:01 -0700

    Update features.jsx

--- Frontend/src/Pages/Home/components/features.jsx
+++ Frontend/src/Pages/Home/components/features.jsx
@@ -7,7 +7,7 @@ const features = () => {
     return (
         <div className="bg-cyan-700 py-20">
             <h1 className='text-center text-white text-3xl mb-24'>أهم المميزات</h1>
-            <div className='max-w-screen-xl mt-5 md:px-10 grid grid-cols-1 md:grid-cols-3 gap-24 mx-auto px-5 md:gap-10'>
+            <div className='max-w-screen-xl mt-5 md:px-10 grid grid-cols-1 md:grid-cols-3 gap-24 mx-auto px-2 md:gap-10'>
                 <div className="feature-wrapper">
                         <img src={repeat} alt="Feature Image" />
                     <h4 className="title">وفر وقت المواصلات</h4>
